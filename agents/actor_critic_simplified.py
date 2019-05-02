1a5cd3d161be4295d0a54e826f2e0ecb34ea43af
MatthewTurney <matthew.w.turney@gmail.com>
2019-05-02 00:56:05 -0700

    added simplified actor critic

---
+++ agents/actor_critic_simplified.py
@@ -0,0 +1,249 @@
+"""Actor-critic agents."""
+import numpy as np
+import os
+import tensorflow as tf
+
+# local submodule
+import agents.networks.policy_value_estimators as nets
+
+from absl import flags
+
+from collections import deque
+
+from pysc2.agents import base_agent
+from pysc2.lib import actions as sc2_actions
+
+FLAGS = flags.FLAGS
+
+# agent interface settings (defaults specified in pysc2.bin.agent)
+feature_screen_size = FLAGS.feature_screen_size
+feature_minimap_size = FLAGS.feature_minimap_size
+
+# pysc2 convenience
+
+FUNCTIONS = sc2_actions.FUNCTIONS
+functions_mask = np.zeros(len(FUNCTIONS))
+allowed_funcs = [12]
+functions_mask[allowed_funcs] = 1
+
+
+FUNCTION_TYPES = sc2_actions.FUNCTION_TYPES
+FunctionCall = sc2_actions.FunctionCall
+
+# manually state the argument types which take points on screen/minimap
+SCREEN_TYPES = [sc2_actions.TYPES[0], sc2_actions.TYPES[2]]
+MINIMAP_TYPES = [sc2_actions.TYPES[1]]
+
+
+class A2CAtariSimplified(base_agent.BaseAgent):
+    """Synchronous version of DeepMind baseline Advantage actor-critic."""
+
+    def __init__(self,
+                 learning_rate=FLAGS.learning_rate,
+                 value_gradient_strength=FLAGS.value_gradient_strength,
+                 regularization_strength=FLAGS.regularization_strength,
+                 discount_factor=FLAGS.discount_factor,
+                 trajectory_training_steps=FLAGS.trajectory_training_steps,
+                 training=FLAGS.training,
+                 save_dir="./checkpoints/",
+                 ckpt_name="A2CAtari",
+                 summary_path="./tensorboard/A2CAtari"):
+        """Initialize rewards/episodes/steps, build network."""
+        super(A2CAtariSimplified, self).__init__()
+
+        # saving and summary writing
+        if FLAGS.save_dir:
+            save_dir = FLAGS.save_dir
+        if FLAGS.ckpt_name:
+            ckpt_name = FLAGS.ckpt_name
+        if FLAGS.summary_path:
+            summary_path = FLAGS.summary_path
+
+        # neural net hyperparameters
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+
+        # agent hyperparameters
+        self.trajectory_training_steps = trajectory_training_steps
+
+        # other parameters
+        self.training = training
+
+        # build network
+        self.save_path = save_dir + ckpt_name + ".ckpt"
+        print("Building models...")
+        tf.reset_default_graph()
+        self.network = nets.AtariNetSimplified(
+            screen_dimensions=feature_screen_size,
+            learning_rate=learning_rate,
+            value_gradient_strength=value_gradient_strength,
+            regularization_strength=regularization_strength,
+            save_path=self.save_path,
+            summary_path=summary_path)
+
+        print("Done.")
+
+        # initialize session
+        self.sess = tf.Session()
+        if os.path.isfile(self.save_path + ".index"):
+            self.network.load(self.sess)
+        else:
+            self._tf_init_op()
+
+    def reset(self):
+        """Handle the beginning of new episodes."""
+        self.episodes += 1
+        self.steps = 0
+        self.reward = 0
+
+        if self.training:
+            self.last_action = None
+            self.state_buffer = deque(maxlen=self.trajectory_training_steps)
+            self.action_buffer = deque(maxlen=self.trajectory_training_steps)
+            self.reward_buffer = deque(maxlen=self.trajectory_training_steps)
+            self.glob_ep = self.network.global_episode.eval(session=self.sess)
+            print("Global training episode:", self.glob_ep + 1)
+
+    def step(self, obs):
+        """If no units selected, selects army, otherwise move."""
+        self.steps += 1
+        self.reward += obs.reward
+
+        # handle end of episode if terminal step
+        if self.training and obs.step_type == 2:
+            self._handle_episode_end()
+
+        # get observations of state
+        observation = obs.observation
+        # expand so they form a batch of 1
+        screen_features = observation.feature_screen.player_relative
+        available_actions = observation.available_actions
+
+
+        # sample action (function identifier and arguments) from policy
+        args = self._sample_action(
+            screen_features,
+            available_actions)
+
+
+        if (self.steps == 1):
+            return FUNCTIONS.select_army("select")
+
+        # train model
+        if self.training:
+            if self.last_action:
+                # most recent steps on the left of the deques
+                self.state_buffer.appendleft((screen_features))
+                self.action_buffer.appendleft(self.last_action)
+                self.reward_buffer.appendleft(obs.reward)
+
+            # cut trajectory and train model
+            if self.steps % self.trajectory_training_steps == 0:
+                self._train_network()
+
+            self.last_action = args
+
+        return FUNCTIONS.Attack_screen("now", args)
+
+    def _sample_action(self,
+                       screen_features,
+                       available_actions):
+
+        """Sample actions and arguments from policy output layers."""
+        screen_features = np.expand_dims(screen_features, 0)
+
+        action_mask = np.zeros(len(FUNCTIONS), dtype=np.int32)
+        for i in range(len(available_actions)):
+            if (available_actions[i] and functions_mask[i]):
+                action_mask[i] = 1
+            else:
+                action_mask[i] = 0
+
+        feed_dict = {self.network.screen_features: screen_features}
+
+        # sample function identifier
+        action_id = 12
+
+        # sample function arguments
+
+        x_policy = self.sess.run(
+                    self.network.x,
+                    feed_dict=feed_dict)
+
+        y_policy = self.sess.run(
+                    self.network.y,
+                    feed_dict=feed_dict)
+
+        x_policy = np.squeeze(x_policy)
+        x_ids = np.arange(len(x_policy))
+        x = np.random.choice(x_ids, p=x_policy)
+
+        y_policy = np.squeeze(y_policy)
+        y_ids = np.arange(len(y_policy))
+        y = np.random.choice(y_ids, p=y_policy)
+        args = (x, y)
+
+
+        #print("CHOSEN ACTION: ", str(action_id), str(args))
+        return args
+
+    def _handle_episode_end(self):
+        """Save weights and write summaries."""
+        # train network
+        feed_dict = self._train_network(terminal=True)
+
+        # increment global training episode
+        self.network.increment_global_episode_op(self.sess)
+
+        # save current model
+        self.network.save_model(self.sess)
+        print("Model Saved")
+
+        # write summaries from last episode
+        self.network.write_summary(
+            self.sess, self.glob_ep, self.reward, feed_dict)
+        print("Summary Written")
+
+    def _tf_init_op(self):
+        init_op = tf.global_variables_initializer()
+        self.sess.run(init_op)
+
+    def _train_network(self, terminal=False):
+        feed_dict = self._get_batch(terminal)
+        self.network.optimizer_op(self.sess, feed_dict)
+        return feed_dict
+
+    def _get_batch(self, terminal):
+        # state
+        screen = [each for each in self.state_buffer]
+
+        # actions and arguments
+        actions = [each[0] for each in self.action_buffer]
+        actions = np.eye(len(FUNCTIONS))[actions]  # one-hot encode actions
+
+        args = self.action_buffer
+        x = np.eye(84)[[a[0] for a in args]]
+        y = np.eye(84)[[a[1] for a in args]]
+
+        # calculate discounted rewards
+        raw_rewards = list(self.reward_buffer)
+        if terminal:
+            value = 0
+        else:
+            value = np.squeeze(self.sess.run(
+                self.network.value_estimate,
+                feed_dict={self.network.screen_features: screen[-1:]}))
+
+        returns = []
+        # n-step discounted rewards from 1 < n < trajectory_training_steps
+        for i, reward in enumerate(raw_rewards):
+            value = reward + self.discount_factor * value
+            returns.append(value)
+
+        feed_dict = {self.network.screen_features: screen,
+                     self.network.actions: actions,
+                     self.network.returns: returns,
+                     self.network.arg_placeholder_x : x,
+                     self.network.arg_placeholder_y : y}
+
+        return feed_dict
--- agents/networks/policy_value_estimators.py
+++ agents/networks/policy_value_estimators.py
@@ -15,6 +15,306 @@ SCREEN_TYPES = [actions.TYPES[0], actions.TYPES[2]]
 MINIMAP_TYPES = [actions.TYPES[1]]
 
 
+class AtariNetSimplified(object):
+    """Estimates value and policy with shared parameters."""
+
+    def __init__(self,
+                 screen_dimensions,
+                 #minimap_dimensions,
+                 learning_rate,
+                 value_gradient_strength,
+                 regularization_strength,
+                 save_path=None,
+                 summary_path=None,
+                 name="AtariNetSimplified"):
+        """Initialize instance-specific hyperparameters, build tf graph."""
+        self.screen_dimensions = screen_dimensions
+        #self.minimap_dimensions = minimap_dimensions
+        self.learning_rate = learning_rate
+        self.value_gradient_strength = value_gradient_strength
+        self.regularization_strength = regularization_strength
+        self.save_path = save_path
+
+        # build graph
+        with tf.variable_scope(name):
+            self._build()
+            self._build_optimization()
+
+        # setup summary writer
+        if summary_path:
+            self.writer = tf.summary.FileWriter(summary_path)
+            tf.summary.scalar("Score", self.score)
+            tf.summary.scalar("Policy_Loss", self.policy_gradient)
+            tf.summary.scalar("Value_Loss", self.value_gradient)
+            tf.summary.scalar("A2C_Loss", self.a2c_gradient)
+            self.write_op = tf.summary.merge_all()
+
+        # setup model saver
+        if self.save_path:
+            self.saver = tf.train.Saver()
+
+    def save_model(self, sess):
+        """Write tensorflow ckpt."""
+        self.saver.save(sess, self.save_path)
+
+    def load(self, sess):
+        """Restore from ckpt."""
+        self.saver.restore(sess, self.save_path)
+
+    def write_summary(self, sess, global_episode, score, feed_dict):
+        """Write summary to Tensorboard."""
+        feed_dict[self.score] = score
+
+        summary = sess.run(
+            self.write_op,
+            feed_dict=feed_dict)
+        self.writer.add_summary(summary, global_episode - 1)
+        self.writer.flush
+
+    def increment_global_episode_op(self, sess):
+        """Increment the global episode tracker."""
+        sess.run(self.increment_global_episode)
+
+    def optimizer_op(self, sess, feed_dict):
+        """Perform one iteration of gradient updates."""
+        sess.run(self.optimizer, feed_dict=feed_dict)
+
+    def _build(self):
+        """Construct graph for state representation."""
+        # score tracker
+        self.score = tf.placeholder(
+            tf.int32,
+            [],
+            name="score")
+
+        # global step traker for multiple runs restoring from ckpt
+        self.global_step = tf.Variable(
+            0,
+            trainable=False,
+            name="global_step")
+
+        self.global_episode = tf.Variable(
+            0,
+            trainable=False,
+            name="global_episode")
+
+        self.increment_global_episode = tf.assign(
+            self.global_episode,
+            self.global_episode + 1,
+            name="increment_global_episode")
+
+        # state placeholders
+        self.screen_features = tf.placeholder(
+            tf.int32,
+            [None, *self.screen_dimensions],
+            name="screen_features")
+
+        #self.minimap_features = tf.placeholder(
+        #    tf.int32,
+        #    [None, len(MINIMAP_FEATURES), *self.minimap_dimensions],
+        #    name="minimap_features")
+
+        #self.flat_features = tf.placeholder(
+        #    tf.float32,
+        #    [None, len(features.Player)],
+        #    name="flat_features")
+
+        # preprocessing
+        self.transposed = tf.transpose(
+                self.screen_features,
+                perm=[0, 2, 1],
+                name="transpose")
+
+            # embed layer (one-hot in channel dimension, 1x1 convolution)
+            # the player_relative feature layer has 5 categorical values
+        self.one_hot = tf.one_hot(
+                self.transposed,
+                depth=5,
+                axis=-1,
+                name="one_hot")
+
+        self.screen_processed = tf.layers.conv2d(
+                inputs=self.one_hot,
+                filters=1,
+                kernel_size=[1, 1],
+                strides=[1, 1],
+                padding="SAME",
+                name="embed")
+
+        #self.minimap_processed = preprocess_spatial_features(
+        #    self.minimap_features,
+        #    screen=False)
+
+        #self.flat_processed = tf.log(
+        #3    self.flat_features + 1.,
+        #    name="flat_processed")
+
+        # convolutional layers for screen features
+        self.screen_conv1 = tf.layers.conv2d(
+            inputs=self.screen_processed,
+            filters=16,
+            kernel_size=[8, 8],
+            strides=[4, 4],
+            padding="SAME",
+            name="screen_conv1")
+
+        self.screen_activation1 = tf.nn.relu(
+            self.screen_conv1,
+            name="screen_activation1")
+
+        self.screen_conv2 = tf.layers.conv2d(
+            inputs=self.screen_activation1,
+            filters=32,
+            kernel_size=[4, 4],
+            strides=[2, 2],
+            padding="SAME",
+            name="screen_conv2")
+
+        self.screen_activation2 = tf.nn.relu(
+            self.screen_conv2,
+            name="screen_activation2")
+
+        # convolutional layers for minimap features
+        #self.minimap_conv1 = tf.layers.conv2d(
+        #    inputs=self.minimap_processed,
+        #    filters=16,
+        #    kernel_size=[8, 8],
+        #    strides=[4, 4],
+        #    padding="SAME",
+        #    name="minimap_conv1")
+
+        #self.minimap_activation1 = tf.nn.relu(
+        #    self.minimap_conv1,
+        #    name="minimap_activation1")
+
+        #self.minimap_conv2 = tf.layers.conv2d(
+        #    inputs=self.minimap_activation1,
+        #    filters=32,
+        #    kernel_size=[4, 4],
+        #    strides=[2, 2],
+        #    padding="SAME",
+        #    name="minimap_conv2")
+
+        #self.minimap_activation2 = tf.nn.relu(
+        #    self.minimap_conv2,
+        #    name="minimap_activation2")
+
+        # linear layer for non-spatial features (tanh activation)
+        #self.flat_linear = tf.layers.dense(
+        #    inputs=self.flat_processed,
+        #    units=64,
+        #    activation=tf.tanh,
+        #    name="flat_linear")
+
+        # flatten and concatenate
+        self.screen_flat = tf.layers.flatten(
+            self.screen_activation2,
+            name="screen_flat")
+
+        #self.minimap_flat = tf.layers.flatten(
+       #     self.minimap_activation2,
+       #     name="minimap_flat")
+
+        #self.concat = tf.concat(
+        #    values=[self.screen_flat, self.minimap_flat, self.flat_linear],
+        #    axis=1,
+        #    name="concat")
+
+        # linear layer with ReLU activation
+        self.state_representation = tf.layers.dense(
+            inputs=self.screen_flat,
+            units=256,
+            activation=tf.nn.relu,
+            name="state_representation")
+
+        units = self.screen_dimensions
+        self.x = tf.layers.dense(
+                    inputs=self.state_representation,
+                    units=units[0],
+                    activation=tf.nn.softmax)
+
+        self.y = tf.layers.dense(
+                    inputs=self.state_representation,
+                    units=units[1],
+                    activation=tf.nn.softmax)
+
+
+        self.arg_placeholder_x = tf.placeholder(
+                    tf.float32,
+                    shape=[None, units[0]],
+                    name="x")
+
+        self.arg_placeholder_y = tf.placeholder(
+                    tf.float32,
+                    shape=[None, units[1]],
+                    name="y")
+
+
+        # value estimation
+        self.value_estimate = tf.layers.dense(
+            inputs=self.state_representation,
+            units=1,
+            activation=None,
+            name="value_estimate")
+
+    def _build_optimization(self):
+        """Construct graph for network updates."""
+        # target placeholders
+        self.actions = tf.placeholder(
+            tf.float32,
+            [None, NUM_ACTIONS],
+            name="actions")
+
+        self.returns = tf.placeholder(
+            tf.float32,
+            [None],
+            name="returns")
+
+
+        self.args_probability = 1.
+
+        arg_prob = tf.reduce_sum(
+                self.arg_placeholder_x * self.x)
+        nonzero_probs = tf.cond(
+                tf.logical_not(tf.equal(arg_prob, 0)),
+                true_fn=lambda: arg_prob,
+                false_fn=lambda: 1.)
+        self.args_probability *= nonzero_probs
+
+        arg_prob = tf.reduce_sum(
+                self.arg_placeholder_y * self.y)
+        nonzero_probs = tf.cond(
+                tf.logical_not(tf.equal(arg_prob, 0)),
+                true_fn=lambda: arg_prob,
+                false_fn=lambda: 1.)
+        self.args_probability *= nonzero_probs
+
+        self.advantage = tf.subtract(
+            self.returns,
+            tf.squeeze(tf.stop_gradient(self.value_estimate)),
+            name="advantage")
+
+        # a2c gradient = policy gradient + value gradient + regularization
+        self.policy_gradient = -tf.reduce_mean(
+            (self.advantage *
+             tf.log(self.args_probability)),
+            name="policy_gradient")
+
+        self.value_gradient = -tf.reduce_mean(
+            self.advantage * tf.squeeze(self.value_estimate),
+            name="value_gradient")
+
+        self.a2c_gradient = tf.add_n(
+            inputs=[self.policy_gradient,
+                    self.value_gradient_strength * self.value_gradient],
+            name="a2c_gradient")
+
+        self.optimizer = tf.train.RMSPropOptimizer(
+            self.learning_rate).minimize(self.a2c_gradient,
+                                         global_step=self.global_step)
+
+
+
 class AtariNet(object):
     """Estimates value and policy with shared parameters."""
 
